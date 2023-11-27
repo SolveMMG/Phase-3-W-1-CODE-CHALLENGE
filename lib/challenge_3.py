f997aa82f05cbb2042253b83abba6b8bc96bb882
Your Name <mikachupichu@gmail.com>
2023-11-27 04:05:01 +0300

    Iterating thru the word and adding consonants

--- lib/challenge_3.py
+++ lib/challenge_3.py
@@ -4,5 +4,23 @@ def solve(word):
     # Creating variable to store vowels
     vowels = "aeiou"
 
-   
+    # Variables to store max value and current value
+    highest_consonant_value = 0
+    consonant_current_value = 0
+
+    # Looping through the word characters.
+    for char in word:
+        # Checking if its a vowel
+        if char not in vowels:
+            # Adding consonant values
+            consonant_current_value += consonant_values[char]
+        # Runs when a vowel is encountered
+        else:
+            # Setting max value
+            highest_consonant_value = max(highest_consonant_value, consonant_current_value)
+            # Reseting current consonant value
+            consonant_current_value = 0
+
+    # Returning max consonant value
+    return highest_consonant_value
 
