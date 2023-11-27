7294aa4f50ff547545e5de591d48bf9a0830efd6
Your Name <mikachupichu@gmail.com>
2023-11-27 04:04:04 +0300

    Arranging alphabets with values

---
+++ lib/challenge_3.py
@@ -0,0 +1,8 @@
+# Defining function
+def solve(word):
+    consonant_values = {chr(i): i - ord('a') + 1 for i in range(ord('a'), ord('z') + 1)}
+    # Creating variable to store vowels
+    vowels = "aeiou"
+
+   
+
